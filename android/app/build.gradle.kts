6ec40679af8f91fe1718c01c55afbcd04403f0c4
Naman Shrimal <namanshrimal12@gmail.com>
2026-08-03 03:51:22 +0530

    Bump Android remote version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.namans12.proxitune"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2.0"
+        versionCode = 3
+        versionName = "0.3.0"
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_21
